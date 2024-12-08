98c0a289f12bed9cf12335ada18d327a963baba8
Austin Sherron <austin@pryvate.email>
2024-12-07 17:14:43 -0800

    [Feature] Update Darwin Bootstrap Script
    * Update aforementioned script to add/update ssh config

--- scripts/hosts/darwin.sh
+++ scripts/hosts/darwin.sh
@@ -4,8 +4,72 @@ set -Eeuo pipefail
 
 
 ANSIBLE_HOSTS="/etc/ansible/hosts"
-SSH_KEY_PATH="$HOME/.ssh/server_0_id_rsa"
 SERVER_0="192.168.12.38"
+
+SSH_KEY_PATH="$HOME/.ssh/server_0_id_rsa"
+SSH_USER="austin"
+
+function install_ansible() {
+    if which ansible &> /dev/null; then
+        echo "[INFO] ansible already installed"
+        return 0
+    fi
+
+    echo "[INFO] installing ansible"
+    brew install ansible
+}
+
+function add_default_ssh_config() {
+    echo "[INFO] Adding default ssh config"
+
+    cat <<EOF >> ~/.ssh/config
+Host *
+    ForwardAgent yes
+    ServerAliveInterval 60
+    ServerAliveCountMax 3
+EOF
+}
+
+function add_server_ssh_config() {
+    echo "[INFO] Adding server ssh config"
+
+    cat <<EOF >> ~/.ssh/config
+
+Host server-0
+    HostName $SERVER_0
+    User $SSH_USER
+    Port 22
+    IdentityFile $SSH_KEY_PATH
+EOF
+}
+
+function update_ssh_config() {
+    if [[ -f ~/.ssh/config ]]; then
+        echo "[INFO] ~/.ssh/config already exists"
+    else
+        add_default_ssh_config
+    fi
+
+    if grep -q "Host server-0" ~/.ssh/config; then
+        echo "[INFO] host server-0 exists in ~/.ssh/config"
+    else
+        add_server_ssh_config
+    fi
+}
+
+function setup_ssh() {
+    if [[ -f "$SSH_KEY_PATH" ]]; then
+        echo "[INFO] ssh key at $SSH_KEY_PATH exists"
+    else
+        echo "[INFO] creating ssh key at $SSH_KEY_PATH"
+        ssh-keygen -t rsa -b 4096 -f "$SSH_KEY_PATH"
+    fi
+
+    echo "[INFO] copying $SSH_KEY_PATH.pub to $SERVER_0"
+    ssh-copy-id -i "$SSH_KEY_PATH.pub" "$SSH_USER@$SERVER_0"
+
+    update_ssh_config
+}
 
 function ansible_hosts_path() {
     echo "$(git rev-parse --show-toplevel)/ansible/hosts"
@@ -30,23 +94,9 @@ function copy_ansible_hosts() {
     fi
 }
 
-function setup_ssh() {
-    if [[ -f "$SSH_KEY_PATH" ]]; then
-        echo "[INFO] ssh key at $SSH_KEY_PATH exists"
-    else
-        echo "[INFO] creating ssh key at $SSH_KEY_PATH"
-        ssh-keygen -t rsa -b 4096 -f "$SSH_KEY_PATH"
-    fi
-
-    echo "[INFO] copying $SSH_KEY_PATH.pub to $SERVER_0"
-    ssh-copy-id -i "$SSH_KEY_PATH.pub" "austin@$SERVER_0"
-}
-
 echo "[INFO] bootstrapping darwin"
 
-echo "[INFO] installing ansible"
-brew install ansible
-
+install_ansible
 copy_ansible_hosts
 setup_ssh
 
