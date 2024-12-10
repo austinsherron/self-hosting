747a13c4acae75fe9a3fbe7053d63240feda7905
Austin Sherron <austin@pryvate.email>
2024-12-09 16:27:08 -0800

    [Feature] Update Nginx Terraform
    * Pass bitnami helm repo as terraform data instead of url

--- infra/nginx/main.tf
+++ infra/nginx/main.tf
@@ -4,17 +4,17 @@ resource "kubernetes_namespace" "nginx" {
   }
 }
 
-# data "helm_repository" "bitnami" {
-#   name = "bitnami"
-#   url  = "https://charts.bitnami.com/bitnami"
-# }
+data "helm_repository" "bitnami" {
+  name = "bitnami"
+  url  = "https://charts.bitnami.com/bitnami"
+}
 
 resource "helm_release" "nginx" {
   name       = "nginx"
   namespace  = kubernetes_namespace.nginx.metadata[0].name
   chart      = "bitnami/nginx"
-  repository = "https://charts.bitnami.com/bitnami"
-  # repository = data.helm_repository.stable.metadata[0].name
+  # repository = "https://charts.bitnami.com/bitnami"
+  repository = data.helm_repository.stable.metadata[0].name
   version = "18.2.6"
   values  = []
 }
