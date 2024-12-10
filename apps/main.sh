a083f505a8caadb33ae0c17fe61b0b34ae8fc10c
Austin Sherron <austin@pryvate.email>
2024-12-10 09:53:19 -0800

    [Feature] Update Nextcloud Values
    * Update nextcloud to use persistent volume claim
    * Update nextcloud data dir
    * Add same play selection mechanism to apps as was recently added to
      infra

--- apps/main.sh
+++ apps/main.sh
@@ -4,12 +4,12 @@ set -Eeuo pipefail
 
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-PLAY="main"
+PLAY=""
 
 export ANSIBLE_BECOME_PASS
 ANSIBLE_BECOME_PASS="$(op item get pduhcnytpl5sw2odxgwi7ptc4m --fields lg7gz3zgxxx5lie4prmu2b52wq)"
 
-[[ $# -gt 0 ]] && PLAY="$1"
+[[ $# -gt 0 ]] && export PLAY="$1"
 
-ansible-playbook "$SCRIPT_DIR/playbooks/$PLAY.yaml"
+ansible-playbook "$SCRIPT_DIR/playbooks/main.yaml"
 
