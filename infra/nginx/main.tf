c7c69631477d397dfd15cf181e6b5891c6377b7c
Austin Sherron <austin@pryvate.email>
2024-12-09 16:32:24 -0800

    [Feature] Update Nginx Terraform
    * Update chart repo from bitnami -> kubernetes github
    * Update chart name

--- infra/nginx/main.tf
+++ infra/nginx/main.tf
@@ -4,17 +4,11 @@ resource "kubernetes_namespace" "nginx" {
   }
 }
 
-data "helm_repository" "bitnami" {
-  name = "bitnami"
-  url  = "https://charts.bitnami.com/bitnami"
-}
-
 resource "helm_release" "nginx" {
   name       = "nginx"
   namespace  = kubernetes_namespace.nginx.metadata[0].name
-  chart      = "bitnami/nginx"
-  # repository = "https://charts.bitnami.com/bitnami"
-  repository = data.helm_repository.stable.metadata[0].name
-  version = "18.2.6"
-  values  = []
+  chart      = "ingress-nginx/ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  version    = "18.2.6"
+  values     = []
 }
